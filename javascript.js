559ca0f3c442f051bf499f14f948403eab06d9fc
kayaba <Stefan.siggaard.andersen@gmail.com>
2023-03-16 22:28:37 +0100

    Changed all "var" to "let"... I forgot Am Dumb

--- javascript.js
+++ javascript.js
@@ -1,23 +1,20 @@
 console.log("Javascript Document Running...")
 
-var columns = 16;
-var rows = 16;
-var grid = document.createElement('div');
-var color = [, "#3C9EE7", "#E7993C",
+let columns = 16;
+let rows = 16;
+let grid = document.createElement('div');
+let color = [, "#3C9EE7", "#E7993C",
     "#E73C99", "#3CE746", "#E7993C"];
 
-
-var grid = document.createElement('div');
 grid.className = 'grid';
-for (var i = 0; i < columns; ++i) {
-    var column = document.createElement('div'); // create column
+
+for (let i = 0; i < columns; i++) {
+    let column = document.createElement('div'); // create column
     column.className = 'column';
-    for (var j = 0; j < rows; ++j) {
-        var row = document.createElement('div'); // create row
+    for (let j = 0; j < rows; j++) {
+        let row = document.createElement('div'); // create row
         row.className = 'row';
         row.textContent = i + '-' + j; // set text
-        
-
         column.appendChild(row); // append row in column
     }
     grid.appendChild(column); // append column inside grid
