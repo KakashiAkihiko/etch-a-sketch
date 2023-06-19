f36587e4b55628f7512117acdd663b56e403c2f7
kayaba <Stefan.siggaard.andersen@gmail.com>
2023-06-19 13:50:42 +0200

    we fixed it boss

--- javascript.js
+++ javascript.js
@@ -3,22 +3,26 @@ console.log("Javascript Document Running...")
 let columns = 16;
 let rows = 16;
 let grid = document.createElement('div');
-let color = [, "#3C9EE7", "#E7993C",
-    "#E73C99", "#3CE746", "#E7993C"];
 
-grid.className = 'grid';
+populateGrid(columns, rows);
 
-for (let i = 0; i < columns; i++) {
-    let column = document.createElement('div'); // create column
-    column.className = 'column';
-    for (let j = 0; j < rows; j++) {
-        let row = document.createElement('div'); // create row
-        row.className = 'row';
-        row.addEventListener('mouseover', () => {
-            row.style.backgroundColor = "black";
-        });
-        column.appendChild(row); // append row in column
+function populateGrid(columns, rows) {
+    let grid = document.createElement('div');
+    grid.className = "grid";
+    
+    for (let i = 0; i < columns; i++) {
+        let column = document.createElement('div'); // create column
+        column.className = 'column';
+        for (let j = 0; j < rows; j++) {
+            let row = document.createElement('div'); // create row
+            row.className = 'row';
+            row.addEventListener('mouseover', () => {
+                row.style.backgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16);;
+            });
+            column.appendChild(row); // append row in column
+        }
+        grid.appendChild(column); // append column inside grid
     }
-    grid.appendChild(column); // append column inside grid
+    document.body.appendChild(grid);
+
 }
-document.body.appendChild(grid);
